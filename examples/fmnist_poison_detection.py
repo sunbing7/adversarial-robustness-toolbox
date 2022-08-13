044a0fcffffd17c3f5e7787f7c53c8aa97f89fa2
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-13 17:47:41 +0800

    code clean up

--- examples/fmnist_poison_detection.py
+++ examples/fmnist_poison_detection.py
@@ -19,68 +19,21 @@ import tensorflow
 from keras.models import load_model
 
 tensorflow.compat.v1.disable_eager_execution()
+RESULT_DIR = 'fmnist'
+TARGET_CLASS = 0
 
 def main():
     # Read MNIST dataset (x_raw contains the original images):
     min_ = 0.
     max_ = 1.
-    x_train_c, y_train_c, x_test_c, y_test_c, x_train_adv, y_train_adv, x_test_adv, y_test_adv, is_poison_train = load_dataset_repair()
-    '''
-    (x_raw, y_raw), (x_raw_test, y_raw_test), min_, max_ = load_mnist(raw=True)
-
-    n_train = np.shape(x_raw)[0]
-    num_selection = 5000
-    random_selection_indices = np.random.choice(n_train, num_selection)
-    x_raw = x_raw[random_selection_indices]
-    y_raw = y_raw[random_selection_indices]
-
-    # Poison training data
-    perc_poison = 0.33
-    (is_poison_train, x_poisoned_raw, y_poisoned_raw) = generate_backdoor(x_raw, y_raw, perc_poison)
-    x_train, y_train = preprocess(x_poisoned_raw, y_poisoned_raw)
-    # Add channel axis:
-    x_train = np.expand_dims(x_train, axis=3)
-
-    # Poison test data
-    (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = generate_backdoor(x_raw_test, y_raw_test, perc_poison)
-    x_test, y_test = preprocess(x_poisoned_raw_test, y_poisoned_raw_test)
-    # Add channel axis:
-    x_test = np.expand_dims(x_test, axis=3)
-
-    # Shuffle training data so poison is not together
-    n_train = np.shape(y_train)[0]
-    shuffled_indices = np.arange(n_train)
-    np.random.shuffle(shuffled_indices)
-    x_train = x_train[shuffled_indices]
-    y_train = y_train[shuffled_indices]
-    is_poison_train = is_poison_train[shuffled_indices]
-    '''
-    '''
-    # Create Keras convolutional neural network - basic architecture from Keras examples
-    # Source here: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=x_train.shape[1:]))
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation="softmax"))
-
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    '''
+    x_train_clean, y_train_clean, x_train_mix, y_train_mix, x_test_mix, y_test_mix, _, _, x_test_adv, y_test_adv, is_poison_train = load_dataset()
     model = load_model('/Users/bing.sun/workspace/Semantic/PyWorkplace/adversarial-robustness-toolbox/examples/fmnist/fmnist_semantic_0_attack.h5')
 
     classifier = KerasClassifier(model=model, clip_values=(min_, max_))
 
-    #classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
-
-    #classifier.save('mnist_1.h5')
-
     # Evaluate the classifier on the test set
-    preds = np.argmax(classifier.predict(x_test_c), axis=1)
-    acc = np.sum(preds == np.argmax(y_test_c, axis=1)) / y_test_c.shape[0]
+    preds = np.argmax(classifier.predict(x_test_mix), axis=1)
+    acc = np.sum(preds == np.argmax(y_test_mix, axis=1)) / y_test_mix.shape[0]
     print("\nTest accuracy: %.2f%%" % (acc * 100))
 
     # Evaluate the classifier on poisonous data
@@ -89,12 +42,12 @@ def main():
     print("\nPoisonous test set accuracy (i.e. effectiveness of poison): %.2f%%" % (acc * 100))
 
     # Evaluate the classifier on clean data
-    preds = np.argmax(classifier.predict(x_train_c), axis=1)
-    acc = np.sum(preds == np.argmax(y_train_c, axis=1)) / y_train_c.shape[0]
+    preds = np.argmax(classifier.predict(x_train_clean), axis=1)
+    acc = np.sum(preds == np.argmax(y_train_clean, axis=1)) / y_train_clean.shape[0]
     print("\nClean test set accuracy: %.2f%%" % (acc * 100))
 
     # Calling poisoning defence:
-    defence = ActivationDefence(classifier, x_train_c, y_train_c)
+    defence = ActivationDefence(classifier, x_train_mix, y_train_mix)
 
     # End-to-end method:
     print("------------------- Results using size metric -------------------")
@@ -112,23 +65,23 @@ def main():
 
     # Visualize clusters:
     print("Visualize clusters")
-    sprites_by_class = defence.visualize_clusters(x_train_c, "fmnist_poison_demo")
+    sprites_by_class = defence.visualize_clusters(x_train_mix, RESULT_DIR + "poison_demo")
     # Show plots for clusters of class 5
-    for n_class in range (0, 10):
-        #n_class = 1
-        try:
-            import matplotlib.pyplot as plt
+    #for n_class in range (0, 10):
+    n_class = TARGET_CLASS
+    try:
+        import matplotlib.pyplot as plt
 
-            plt.imshow(sprites_by_class[n_class][0])
-            plt.title("Class " + str(n_class) + " cluster: 0")
-            #plt.show()
-            plt.savefig("fmnist/Class " + str(n_class) + " cluster: 0" + ".png")
-            plt.imshow(sprites_by_class[n_class][1])
-            plt.title("Class " + str(n_class) + " cluster: 1")
-            #plt.show()
-            plt.savefig("fmnist/Class " + str(n_class) + " cluster: 1" + ".png")
-        except ImportError:
-            print("matplotlib not installed. For this reason, cluster visualization was not displayed")
+        plt.imshow(sprites_by_class[n_class][0])
+        plt.title("Class " + str(n_class) + " cluster: 0")
+        #plt.show()
+        plt.savefig("fmnist/Class " + str(n_class) + " cluster: 0" + ".png")
+        plt.imshow(sprites_by_class[n_class][1])
+        plt.title("Class " + str(n_class) + " cluster: 1")
+        #plt.show()
+        plt.savefig("fmnist/Class " + str(n_class) + " cluster: 1" + ".png")
+    except ImportError:
+        print("matplotlib not installed. For this reason, cluster visualization was not displayed")
 
     '''
     # Try again using distance analysis this time:
@@ -156,64 +109,7 @@ def main():
     '''
     print("done :) ")
 
-
-def generate_backdoor(
-        x_clean, y_clean, percent_poison, backdoor_type="pattern", sources=np.arange(10), targets=(np.arange(10) + 1) % 10
-):
-    """
-    Creates a backdoor in MNIST images by adding a pattern or pixel to the image and changing the label to a targeted
-    class. Default parameters poison each digit so that it gets classified to the next digit.
-
-    :param x_clean: Original raw data
-    :type x_clean: `np.ndarray`
-    :param y_clean: Original labels
-    :type y_clean:`np.ndarray`
-    :param percent_poison: After poisoning, the target class should contain this percentage of poison
-    :type percent_poison: `float`
-    :param backdoor_type: Backdoor type can be `pixel` or `pattern`.
-    :type backdoor_type: `str`
-    :param sources: Array that holds the source classes for each backdoor. Poison is
-    generating by taking images from the source class, adding the backdoor trigger, and labeling as the target class.
-    Poisonous images from sources[i] will be labeled as targets[i].
-    :type sources: `np.ndarray`
-    :param targets: This array holds the target classes for each backdoor. Poisonous images from sources[i] will be
-                    labeled as targets[i].
-    :type targets: `np.ndarray`
-    :return: Returns is_poison, which is a boolean array indicating which points are poisonous, x_poison, which
-    contains all of the data both legitimate and poisoned, and y_poison, which contains all of the labels
-    both legitimate and poisoned.
-    :rtype: `tuple`
-    """
-
-    max_val = np.max(x_clean)
-
-    x_poison = np.copy(x_clean)
-    y_poison = np.copy(y_clean)
-    is_poison = np.zeros(np.shape(y_poison))
-
-    for i, (src, tgt) in enumerate(zip(sources, targets)):
-        n_points_in_tgt = np.size(np.where(y_clean == tgt))
-        num_poison = round((percent_poison * n_points_in_tgt) / (1 - percent_poison))
-        src_imgs = x_clean[y_clean == src]
-
-        n_points_in_src = np.shape(src_imgs)[0]
-        indices_to_be_poisoned = np.random.choice(n_points_in_src, num_poison)
-
-        imgs_to_be_poisoned = np.copy(src_imgs[indices_to_be_poisoned])
-        if backdoor_type == "pattern":
-            imgs_to_be_poisoned = add_pattern_bd(x=imgs_to_be_poisoned, pixel_value=max_val)
-        elif backdoor_type == "pixel":
-            imgs_to_be_poisoned = add_single_bd(imgs_to_be_poisoned, pixel_value=max_val)
-        x_poison = np.append(x_poison, imgs_to_be_poisoned, axis=0)
-        y_poison = np.append(y_poison, np.ones(num_poison) * tgt, axis=0)
-        is_poison = np.append(is_poison, np.ones(num_poison))
-
-        is_poison = is_poison != 0
-
-        return is_poison, x_poison, y_poison
-
-
-def load_dataset_repair():
+def load_dataset():
     '''
     split test set: first half for fine tuning, second half for validation
     @return
@@ -248,7 +144,7 @@ def load_dataset_repair():
     idx = np.arange(len(x_clean))
     np.random.shuffle(idx)
 
-    print(idx)
+    #print(idx)
 
     x_clean = x_clean[idx, :]
     y_clean = y_clean[idx, :]
@@ -256,26 +152,32 @@ def load_dataset_repair():
     idx = np.arange(len(x_adv))
     np.random.shuffle(idx)
 
-    print(idx)
+    #print(idx)
+
+    DATA_SPLIT = 0.3
 
     x_adv = x_adv[idx, :]
     y_adv_c = y_adv_c[idx, :]
     #'''
 
-    x_train_c = np.concatenate((x_clean[int(len(x_clean) * 0.5):], x_adv[int(len(x_adv) * 0.3):]), axis=0)
-    y_train_c = np.concatenate((y_clean[int(len(y_clean) * 0.5):], y_adv_c[int(len(y_adv_c) * 0.3):]), axis=0)
+    x_train_clean = x_clean[int(len(x_clean) * DATA_SPLIT):]
+    y_train_clean = y_clean[int(len(y_clean) * DATA_SPLIT):]
 
-    x_test_c = np.concatenate((x_clean[:int(len(x_clean) * 0.5)], x_adv[:int(len(x_adv) * 0.3)]), axis=0)
-    y_test_c = np.concatenate((y_clean[:int(len(y_clean) * 0.5)], y_adv_c[:int(len(y_adv_c) * 0.3)]), axis=0)
+    x_train_mix = np.concatenate((x_clean[int(len(x_clean) * DATA_SPLIT):], x_adv[int(len(x_adv) * DATA_SPLIT):]), axis=0)
+    y_train_mix = np.concatenate((y_clean[int(len(y_clean) * DATA_SPLIT):], y_adv_c[int(len(y_adv_c) * DATA_SPLIT):]), axis=0)
 
-    is_poison_train = np.append(np.zeros(len(x_clean[int(len(x_clean) * 0.5):])), np.ones(len(x_adv[int(len(x_adv) * 0.3):])), axis=0)
+    x_test_mix = np.concatenate((x_clean[:int(len(x_clean) * DATA_SPLIT)], x_adv[:int(len(x_adv) * DATA_SPLIT)]), axis=0)
+    y_test_mix = np.concatenate((y_clean[:int(len(y_clean) * DATA_SPLIT)], y_adv_c[:int(len(y_adv_c) * DATA_SPLIT)]), axis=0)
 
-    x_train_adv = x_adv[int(len(y_adv) * 0.3):]
-    y_train_adv = y_adv[int(len(y_adv) * 0.3):]
-    x_test_adv = x_adv[:int(len(y_adv) * 0.3)]
-    y_test_adv = y_adv[:int(len(y_adv) * 0.3)]
+    # is poison in x_train_mix
+    is_poison_train = np.append(np.zeros(len(x_clean[int(len(x_clean) * DATA_SPLIT):])), np.ones(len(x_adv[int(len(x_adv) * DATA_SPLIT):])), axis=0)
 
-    return x_train_c, y_train_c, x_test_c, y_test_c, x_train_adv, y_train_adv, x_test_adv, y_test_adv, is_poison_train
+    x_train_adv = x_adv[int(len(y_adv) * DATA_SPLIT):]
+    y_train_adv = y_adv[int(len(y_adv) * DATA_SPLIT):]
+    x_test_adv = x_adv[:int(len(y_adv) * DATA_SPLIT)]
+    y_test_adv = y_adv[:int(len(y_adv) * DATA_SPLIT)]
+
+    return x_train_clean, y_train_clean, x_train_mix, y_train_mix, x_test_mix, y_test_mix, x_train_adv, y_train_adv, x_test_adv, y_test_adv, is_poison_train
 
 
 if __name__ == "__main__":
